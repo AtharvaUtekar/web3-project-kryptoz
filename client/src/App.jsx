25a7a85d273b3fdd3708f5e375512bb787916ef4
AtharvaUtekar <atharva.v.utekar@gmail.com>
2022-09-21 22:57:03 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { Navbar, Footer, Services, Welcome, Transactions } from './components'
+
+const App = () => {
+  return (
+    <div>
+      <div className="min h-screen">
+          <div className="gradient-bg-welcome">
+            <Navbar />
+            <Welcome />
+          </div>
+          <Services />
+          <Transactions />
+          <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
