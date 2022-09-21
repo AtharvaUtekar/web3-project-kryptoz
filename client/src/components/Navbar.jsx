9a94c984de03354386665098f7641aee3e56d5d6
AtharvaUtekar <atharva.v.utekar@gmail.com>
2022-09-21 23:13:05 +0530

    Assests Loaded

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { HiOutlineMenu } from "react-icons/hi"
+import { AiOutlineClose } from "react-icons/ai"
+
+import logo from '../../images/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Heloooo</div>
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+
+    </nav>
   )
 }
 
