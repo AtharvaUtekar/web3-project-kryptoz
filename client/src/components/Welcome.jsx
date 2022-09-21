0a705d64f70850f26c55db27f0e8b6af421373a0
AtharvaUtekar <atharva.v.utekar@gmail.com>
2022-09-22 01:47:24 +0530

    Hero page initial

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,7 +3,6 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
-import {Loader} from './'
 
 const Welcome = () => {
   const connectWallet = () => {
