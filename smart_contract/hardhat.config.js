f3f30a8a78720bdff13e5e6494e7b6149d393893
AtharvaUtekar <atharva.v.utekar@gmail.com>
2022-09-30 09:39:36 +0530

    Scripts final commit I

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/RUM21tWJcPytE7Wog3NT9SXAEsh7Ro3i
 
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.17",
-};
+require('@nomiclabs/hardhat-waffle');
+
+module.exports ={
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/RUM21tWJcPytE7Wog3NT9SXAEsh7Ro3i',
+      account: ['837cd75618c29c1f0ba0731feb33b4cd550a699007aed14637b8197f5ff871f2']
+    }
+  }
+}
+
