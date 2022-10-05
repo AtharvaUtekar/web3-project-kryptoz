e438c782fa53695c06bd2723dcf399df6a5ced19
AtharvaUtekar <atharva.v.utekar@gmail.com>
2022-10-05 18:59:41 +0530

    Contract link done

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+//0xC1Da4a664251F6183CE74C5ACC8eaC161CCB38C6
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0xC1Da4a664251F6183CE74C5ACC8eaC161CCB38C6';
