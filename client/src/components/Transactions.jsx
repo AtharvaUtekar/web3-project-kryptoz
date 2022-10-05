e0b5ce1780d8c3bb9a34be2da1bbd84d7b0b6048
AtharvaUtekar <atharva.v.utekar@gmail.com>
2022-10-06 04:06:44 +0530

    Final Commit

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -56,14 +56,14 @@ const Transactions = () => {
   return (
     <div className="flex-row w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
       <div className="flex flex-col md:p-12 py-12 px-4">
-      {currentAccount? <h3 className="text-white text-3xl text-center my-2">Your Latest Transactions</h3>:
+      {currentAccount? <h3 className="text-white text-3xl text-center my-2">Your Past Transactions</h3>:
       <h3 className="text-white text-3xl text-center my-2">Connect your account to see the latest transactions</h3>
       }
       </div>
 
       <div className="flex flex-wrap justify-center items-center mt-10 m-3">
       {
-        transactions.reverse().map((transaction,i) => (
+        (transactions.slice(0)).map((transaction,i) => (
           <TransactionsCard key={i} {...transaction}/>
         ))    
       }
